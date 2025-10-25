14f3f54bfe0b29cc949c1a0564c604cb6ba63362
Niranjan Poddar <kumar.niranjan02@outlook.com>
2025-10-25 15:46:56 +0530

    Added and modified controllers

---
+++ Contract/IPersonService.cs
@@ -0,0 +1,13 @@
+﻿using SmsAPI.DTOs;
+using SmsAPI.Models;
+
+namespace SmsAPI.Contract
+{
+    public interface IPersonService
+    {
+        Task<Person> GetPersonById(int id);
+        Task<List<Person>> GetPersonAll();
+        Task<List<PersonDto>> GetPersonAllCache();
+        Task UpdatePerson(Person person);
+    }
+}
---
+++ Controllers/PersonV2Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmsAPI.Contract;
+
+namespace SmsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonV2Controller : ControllerBase
+    {
+        private readonly ILogger<PersonTwoController> _logger;
+        private readonly IPersonService _personService;
+    }
+}
